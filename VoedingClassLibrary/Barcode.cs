8f45ec2b1baca8349b26a0973ed796ec1a619fe6
Daniel Krois <danielkrois@hotmail.com>
2022-01-18 14:49:50 +0100

    added class library

---
+++ VoedingClassLibrary/Barcode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoedingClassLibrary
+{
+    public class Barcode
+    {
+        public int Id { get; set; }
+        public string InputField { get; set; }
+        public double Gewicht { get; set; }
+
+
+
+    }
+}
