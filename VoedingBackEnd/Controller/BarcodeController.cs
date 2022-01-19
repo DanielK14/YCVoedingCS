3b018c5ec8c1e4079d667c782459920192bbe046
Daniel Krois <danielkrois@hotmail.com>
2022-01-19 16:18:54 +0100

    added controller

---
+++ VoedingBackEnd/Controller/BarcodeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VoedingClassLibrary;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VoedingBackEnd.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BarcodeController : ControllerBase
+    {
+        DatabaseContext nieuweBarcode;
+        public BarcodeController(DatabaseContext databaseContext)
+        {
+            nieuweBarcode = databaseContext;
+        }
+        // GET: api/<BarcodeController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<BarcodeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<BarcodeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BarcodeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BarcodeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
